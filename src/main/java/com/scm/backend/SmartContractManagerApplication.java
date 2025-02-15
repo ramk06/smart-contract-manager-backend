da59dd0800b9d7c3fb2b1d0f57bdea82e264ba8e
ramk06 <ramoffical06@gmail.com>
2025-02-15 19:29:31 +0530

    Created the structure

---
+++ src/main/java/com/scm/backend/SmartContractManagerApplication.java
@@ -0,0 +1,13 @@
+package com.scm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContractManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContractManagerApplication.class, args);
+	}
+
+}
